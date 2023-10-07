4b9b83d0b672fa4584a1204a5f92572dc14c51b5
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-10-07 22:25:49 +0300

    Add collate

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -3,6 +3,8 @@ from typing import List
 
 logger = logging.getLogger(__name__)
 
+import torch
+import torch.nn.functional as F
 
 def collate_fn(dataset_items: List[dict]):
     """
@@ -10,5 +12,28 @@ def collate_fn(dataset_items: List[dict]):
     """
 
     result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+
+    values_to_pad = ['spectrogram', 'text_encoded', 'audio']
+
+    for value in values_to_pad:
+        lengths = [item[value].shape[-1] for item in dataset_items]
+        result_batch[f'{value}_length'] = torch.tensor(lengths)
+
+        size_to_pad = max(lengths)
+        result_batch[value] = torch.cat([
+            F.pad(
+                input = item[value], 
+                pad = (0, size_to_pad - item[value].shape[-1]),
+                value = 0
+            ) 
+            for item in dataset_items
+        ])
+
+    result_batch['text'] = []
+    result_batch['audio_path'] = []
+
+    for item in dataset_items:
+        result_batch['text'].append(item['text'])
+        result_batch['audio_path'].append(item['audio_path'])
+
+    return result_batch
