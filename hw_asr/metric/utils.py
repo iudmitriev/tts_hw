f74f61f4a8f55405edc57a20115dde8367d48bdc
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-10-07 22:27:04 +0300

    Add cer and wer

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,13 @@
 # Don't forget to support cases when target_text == ''
+import editdistance
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if target_text == '':
+        return int(predicted_text == '')
+    target_text_splitted = target_text.split(' ')
+    predicted_text_splitted = predicted_text.split(' ')
+    return editdistance.eval(target_text_splitted, predicted_text_splitted) / len(target_text_splitted)
