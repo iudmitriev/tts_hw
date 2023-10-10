ec9847a9fd550b512d61c41b9d538fc38edad58c
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-10-10 20:42:02 +0300

    Add DeepSpeech2

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,5 +1,7 @@
 from hw_asr.model.baseline_model import BaselineModel
+from hw_asr.model.deepspeech2 import DeepSpeech2Model
 
 __all__ = [
     "BaselineModel",
+    "DeepSpeech2Model"
 ]
---
+++ hw_asr/model/deepspeech2.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+from einops import rearrange 
+
+from hw_asr.base import BaseModel
+
+
+class DeepSpeech2Model(BaseModel):
+    def __init__(self, n_feats, n_class, hidden_channels=32, gru_hidden_size=800, gru_num_layers=5, **batch):
+        super().__init__(n_feats, n_class, **batch)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=hidden_channels, kernel_size=(11, 41), stride=1, padding='same'),
+            nn.BatchNorm2d(num_features=hidden_channels),
+            nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=(11, 21), stride=1, padding='same')
+        )
+        self.rnn = nn.GRU(input_size=hidden_channels*n_feats, hidden_size=gru_hidden_size, num_layers=gru_num_layers, batch_first=True)
+
+        self.full = nn.Linear(in_features=gru_hidden_size, out_features=n_class)
+
+
+    def forward(self, spectrogram, **batch):
+        x = spectrogram.unsqueeze(dim=1)
+        x = self.conv(x)
+        x = rearrange(x, 'b c f l -> b l (c f)')
+        x = self.rnn(x)[0]
+        x = self.full(x)
+        return {"logits": x}
+
+    def transform_input_lengths(self, input_lengths):
+        return input_lengths  # we don't reduce time dimension here
