572295589ab8af60faedb0d9fe780cc83df6b6e3
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-10-23 12:32:21 +0300

    Add bidirectional GRU for deepspeech2

--- hw_asr/model/deepspeech2.py
+++ hw_asr/model/deepspeech2.py
@@ -7,7 +7,7 @@ from hw_asr.base import BaseModel
 
 
 class DeepSpeech2Model(BaseModel):
-    def __init__(self, n_feats, n_class, hidden_channels=32, gru_hidden_size=800, gru_num_layers=5, **batch):
+    def __init__(self, n_feats, n_class, hidden_channels=32, gru_hidden_size=800, gru_num_layers=5, bidirectional=False, linear_hidden_size=1600, **batch):
         super().__init__(n_feats, n_class, **batch)
 
         self.conv = nn.Sequential(
@@ -15,10 +15,17 @@ class DeepSpeech2Model(BaseModel):
             nn.BatchNorm2d(num_features=hidden_channels),
             nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=(11, 21), stride=1, padding='same')
         )
-        self.rnn = nn.GRU(input_size=hidden_channels*n_feats, hidden_size=gru_hidden_size, num_layers=gru_num_layers, batch_first=True)
+        self.rnn = nn.GRU(input_size=hidden_channels*n_feats, hidden_size=gru_hidden_size, num_layers=gru_num_layers, 
+                          bidirectional=bidirectional, batch_first=True)
 
-        self.full = nn.Linear(in_features=gru_hidden_size, out_features=n_class)
-
+        if bidirectional:
+            linear_in_features = 2 * gru_hidden_size
+        else:
+            linear_in_features = gru_hidden_size
+        self.full = nn.Sequential(
+            nn.Linear(in_features=linear_in_features, out_features=linear_hidden_size),
+            nn.Linear(in_features=linear_hidden_size, out_features=n_class),
+        )
 
     def forward(self, spectrogram, **batch):
         x = spectrogram.unsqueeze(dim=1)
