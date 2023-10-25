0b880c15e0bd28ed4222102902f771be9effeed8
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-10-25 20:15:48 +0300

    Update configs

---
+++ install_script.sh
@@ -0,0 +1,16 @@
+mkdir lm
+cd lm
+git clone https://github.com/kpu/kenlm
+cd kenlm
+mkdir build
+cd build
+sudo cmake ..
+sudo make -j 4
+sudo make install
+
+cd ..
+cd ..
+mkdir models
+cd models
+wget https://www.openslr.org/resources/11/3-gram.arpa.gz
+gzip -d 3-gram.arpa.gz
