7adc35c31e05aa35bfa40665d6a70694623b1a45
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-10-27 17:56:45 +0300

    Add way to download best model

---
+++ download_best_model.sh
@@ -0,0 +1,7 @@
+mkdir best_model
+cd best_model
+
+gdown https://drive.google.com/uc\?id\=1UGwWBSCt-IriYFmhL2TUqjSa6WnLZbu7
+gdown https://drive.google.com/uc\?id\=1CBFgmnHvemRj5PNFHtyeVJ2RPOJ4gDmH
+
+cd ..
