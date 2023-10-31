a0e21a1612558b2ecf1680003a105ad8f0a80260
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-10-31 15:42:54 +0300

    Remove model download

--- download_best_model.sh
+++
@@ -1,7 +0,0 @@
-mkdir default_test_model
-cd default_test_model
-
-gdown https://drive.google.com/uc\?id\=1UGwWBSCt-IriYFmhL2TUqjSa6WnLZbu7
-gdown https://drive.google.com/uc\?id\=1CBFgmnHvemRj5PNFHtyeVJ2RPOJ4gDmH
-
-cd ..
